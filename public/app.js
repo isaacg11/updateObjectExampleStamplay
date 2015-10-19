5172ef1cdde6672f649e94b4a61d3f0e7c6bc5b6
isaacg11 <isaacgreywebdeveloper@gmail.com>
2015-10-18 20:32:46 -0700

    update object works

--- public/app.js
+++ public/app.js
@@ -4,8 +4,7 @@ window.onload = function(){
 	var objectInstance = new Stamplay.Cobject('object').Model;
 	objectInstance.fetch('56244f3f43c9f31b462537ce').then(function() {
 	var name = objectInstance.get('description');
-	console.log(name);
-	var date = objectInstance.get('dt_create');
+	var date = objectInstance.get('dt_update');
 	var id = objectInstance.get('_id');
 	
 	document.getElementById('objectOutputName').innerHTML = name;
@@ -13,3 +12,22 @@ window.onload = function(){
 	document.getElementById('objectOutputID').innerHTML = id;
 	});
 };
+
+function updateObject(){
+	var id = document.getElementById('objectOutputID').innerHTML;
+	var newName = document.getElementById('name').value;
+	var objectInstance = new Stamplay.Cobject('object').Model;
+
+	objectInstance.fetch(id).then(function(){
+    objectInstance.set('description', newName);
+    objectInstance.save();
+    var description = objectInstance.get('description');
+	var date = objectInstance.get('dt_update');
+
+	document.getElementById('objectOutputName').innerHTML = description;
+	document.getElementById('objectOutputDate').innerHTML = date;
+	document.getElementById('name').value = "";
+
+	});
+}
+
